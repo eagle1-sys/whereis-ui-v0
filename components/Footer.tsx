d885441851b5f8496d7daff5ee2b27e09014f9dd
Panos Samiotis <psamiotis@gmail.com>
2026-04-02 12:09:44 +0300

    Update footer version to v0.3.2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ interface FooterProps {
 export default function Footer({ class: className = "py-8" }: FooterProps) {
   return (
     <footer class={`${className} text-center text-xs`}>
-      <a href="https://github.com/eagle1-sys/whereis-ui-v0" target="_blank" class="underline hover:no-underline">v0.3.1</a> ~ Powered by <a href="https://github.com/eagle1-sys" target="_blank" class="underline hover:no-underline">Eagle1 Whereis</a>
+      <a href="https://github.com/eagle1-sys/whereis-ui-v0" target="_blank" class="underline hover:no-underline">v0.3.2</a> ~ Powered by <a href="https://github.com/eagle1-sys" target="_blank" class="underline hover:no-underline">Eagle1 Whereis</a>
     </footer>
   );
 }
